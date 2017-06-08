2db87324dc0e8fdd208273e20d6628ad34f0cb6b
Farmovich <constant.mntn@gmail.com>
2017-06-08 17:24:45 +0200

    Add files via upload

---
+++ onessaye.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+#script audio.py
+
+import wave
+import math
+import binascii
+import pymedia
+
+player = pymedia.Player()
+player.start()
+player.startPlayback('DNCE.mp3')
+while player.isPlaying()
+time.sleep(0.01)
